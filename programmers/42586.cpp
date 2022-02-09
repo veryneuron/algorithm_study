b7f31a9c09704c1f7a571d83ba47619f151b4e27
JaeMin An <veryneuron1@gmail.com>
2022-02-09 14:44:17 +0900

    Understand question : 스택/큐 - 기능개발

--- programmers/42586.cpp
+++ programmers/42586.cpp
@@ -1,46 +1,42 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <deque>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
 
-    vector<int> round, temp;
-    vector<vector<int>> stack;
+    deque<int> round;
     vector<int> answer;
-
-    int index(0),counter(0);
 
     for (int i = 0; i < progresses.size(); ++i) {
         round.push_back((100 - progresses[i]) / speeds[i]);
         if (((100-progresses[i]) % speeds[i]) != 0) round[i]++;
     }
 
-    while (index < round.size()) {
+    while (!round.empty()) {
+        int temp = round.front();
+        int func(1);
+        round.pop_front();
 
-        temp.push_back(round[index]);
-        counter = round[index];
-        if (++index < round.size()) {
-            while(round[index] <= counter) {
-                temp.push_back(round[index]);
-                counter = counter - round[index];
-                index++;
-            }
+        auto itr = round.begin();
+        
+        while ((!round.empty()) && (round.front() <= temp)) {
+            round.pop_front();
+            func++;
         }
-        stack.push_back(temp);
-        temp.clear();
+        
+        answer.push_back(func);
     }
     
-
-    for (auto result : stack) answer.push_back(result.size());
 
     return answer;
 }
 
 int main() {
-    vector<int> progress = {93, 30, 55};
-    vector<int> speed = {1, 30, 5};
+    vector<int> progress = {95, 90, 99, 99, 80, 99};
+    vector<int> speed = {1, 1, 1, 1, 1, 1};
 
     for (auto i : solution(progress, speed)) cout<< i << endl;
 

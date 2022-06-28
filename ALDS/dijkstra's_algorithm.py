47977ccf16970c85d76ffbb4ea9d0982595675f2
veryneuron <veryneuron1@gmail.com>
2022-06-28 23:53:57 +0900

    dijkstra's algorithm copy

---
+++ ALDS/dijkstra's_algorithm.py
@@ -0,0 +1,66 @@
+# dijkstra's algorithm implementation
+#
+# code from stackabuse
+# while remain graph is exsist
+# extract minimum distance vertex from graph and add to shortest path
+# do relax
+
+from queue import PriorityQueue
+
+class Graph:
+    def __init__(self, num_of_vertices):
+        self.v = num_of_vertices
+        self.edges = [[-1 for i in range(num_of_vertices)] for j in range(num_of_vertices)]
+        self.visited = []
+
+    def add_edge(self, u, v, weight):
+        self.edges[u][v] = weight
+        self.edges[v][u] = weight
+    
+def dijkstra(graph, start_vertex):
+    D = {v:float('inf') for v in range(graph.v)}
+    D[start_vertex] = 0
+    
+    pq = PriorityQueue()
+    pq.put((0, start_vertex))
+
+    while not pq.empty():
+        # extract minimum and goto that node
+        (dist, current_vertex) = pq.get()
+        graph.visited.append(current_vertex)
+
+        # relax adjustment node
+        for neighbor in range(graph.v):
+            if graph.edges[current_vertex][neighbor] != -1:
+                distance = graph.edges[current_vertex][neighbor]
+                if neighbor not in graph.visited:
+                    old_cost = D[neighbor]
+                    new_cost = D[current_vertex] + distance
+                    if new_cost < old_cost:
+                        pq.put((new_cost, neighbor))
+                        D[neighbor] = new_cost
+
+    return D
+
+# driver
+g = Graph(9)
+g.add_edge(0, 1, 4)
+g.add_edge(0, 6, 7)
+g.add_edge(1, 6, 11)
+g.add_edge(1, 7, 20)
+g.add_edge(1, 2, 9)
+g.add_edge(2, 3, 6)
+g.add_edge(2, 4, 2)
+g.add_edge(3, 4, 10)
+g.add_edge(3, 5, 5)
+g.add_edge(4, 5, 15)
+g.add_edge(4, 7, 1)
+g.add_edge(4, 8, 5)
+g.add_edge(5, 8, 12)
+g.add_edge(6, 7, 1)
+g.add_edge(7, 8, 3) 
+
+D = dijkstra(g, 0)
+
+print(D)
+# {0: 0, 1: 4, 2: 11, 3: 17, 4: 9, 5: 22, 6: 7, 7: 8, 8: 11}

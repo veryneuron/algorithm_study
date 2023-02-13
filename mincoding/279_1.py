b35b7ece01de595d761bdf8ab1df47f0f0e83fda
veryneuron <veryneuron@naver.com>
2023-02-13 23:33:22 +0900

    마법의 물뿌리기

--- mincoding/279_1.py
+++ mincoding/279_1.py
@@ -1,41 +1,39 @@
 # 홀수날 키1성장, 짝수날 키2성장, 안뿌릴수도있음
 # 완전탐색... - x! 그리디?
+# ㄴx! 2로 나눠 2의 개수 찾은 뒤 2개수와 1개수 비슷하게 2를 1로 쪼개줌!
 
-import sys
-
-min_days = sys.maxsize
-
-def dfs(water_list, days, is_odd):
-    global min_days
-    # print(f"water list - {water_list}, days - {days}")
-    if days > min_days:
-        return
-    if sum(water_list) == 0:
-        min_days = min(days, min_days)
-    for i in range(len(water_list)):
-        if is_odd:
-            if water_list[i] > 0:
-                if water_list[i] >= 2:
-                    dfs(water_list, days+1, not is_odd)
-                water_list[i] -= 1
-                dfs(water_list, days+1, not is_odd)
-                water_list[i] += 1
-        else:
-            if water_list[i] >= 2:
-                water_list[i] -= 2
-                dfs(water_list, days+1, not is_odd)
-                water_list[i] += 2
-            elif water_list[i] > 0:
-                dfs(water_list, days+1, not is_odd)
 
 TC = int(input())
 answer = []
 for _ in range(TC):
     N = int(input())
     heights = list(map(int, input().split()))
-    min_days = sys.maxsize
     water_list = list(map(lambda x: max(heights) - x, heights))
-    dfs(water_list, 0, True)
-    answer.append(min_days)
+    num_of_2, num_of_1 = 0, 0
+    for water in water_list:
+        if water > 2:
+            if (water % 2) == 1:
+                num_of_1 += 1
+            num_of_2 += water // 2
+        elif water == 2:
+            num_of_2 += 1
+        elif water == 1:
+            num_of_1 += 1
+    # print(f"num1={num_of_1}, num2={num_of_2}")
+    if num_of_1 > num_of_2:
+        answer.append(num_of_1 * 2 - 1)
+    elif num_of_1 == num_of_2:
+        answer.append(num_of_1 * 2)
+    else:
+        diff = num_of_2 - num_of_1
+        # print(f"diff={diff}")
+        if (diff % 3) == 2:
+            cali = (diff // 3) + 1
+            # print(f"cali={cali}")
+            answer.append((num_of_1 + cali * 2) * 2 - 1)
+        else:
+            cali = diff // 3
+            answer.append((num_of_2 - cali) * 2)
+
 for tc in range(TC):    
     print(f'#{tc+1} {answer[tc]}')
